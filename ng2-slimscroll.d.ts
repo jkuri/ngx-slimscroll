ebf111b6405726fa447d9dc313907c1c298e4ddc
jkuri <jkuri88@gmail.com>
2016-01-11 08:23:33 +0100

    Removed files in gitignore

--- ng2-slimscroll.d.ts
+++
@@ -1,6 +0,0 @@
-import { SlimScroll } from './src/directives/slimscroll.directive';
-export * from './src/directives/slimscroll.directive';
-declare var _default: {
-    directives: typeof SlimScroll[];
-};
-export default _default;
