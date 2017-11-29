3fa7a47c4324a25778b8f09e754c77c37080865a
fguerreiro <fabio.guerreiro@omnibees.com>
2017-11-29 15:51:58 +0000

    fix packaging process for NPM publish

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,5 +1,3 @@
-import { NgSlimScrollModule } from './app/ngx-slimscroll/module/ngx-slimscroll.module';
-import { ISlimScrollOptions } from './app/ngx-slimscroll/classes/slimscroll-options.class';
-import { SlimScrollEvent } from './app/ngx-slimscroll/classes/slimscroll-event.class';
-
-export { NgSlimScrollModule, ISlimScrollOptions, SlimScrollEvent };
+export * from './app/ngx-slimscroll/classes/slimscroll-event.class';
+export * from './app/ngx-slimscroll/classes/slimscroll-options.class';
+export * from './app/ngx-slimscroll/module/ngx-slimscroll.module';
